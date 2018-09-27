f08f39ff7b1ede363b6cb317c51cfe418f946f12
Eguzkiman <eguzki@kioru.com>
2018-09-27 17:36:28 -0500

    created repo & added kittens scaffold

---
+++ db/migrate/20180927223600_create_kittens.rb
@@ -0,0 +1,12 @@
+class CreateKittens < ActiveRecord::Migration[5.2]
+  def change
+    create_table :kittens do |t|
+      t.string :name
+      t.integer :cuteness
+      t.string :owner
+      t.boolean :explodes
+
+      t.timestamps
+    end
+  end
+end
